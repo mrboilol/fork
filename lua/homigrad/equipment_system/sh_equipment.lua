8a7ffb813e43328eb3d239bcc33abb5974e4cddd
sadsalat <74601305+sadsalat@users.noreply.github.com>
2026-09-24 20:37:05 +0300

    and again

--- lua/homigrad/equipment_system/sh_equipment.lua
+++ lua/homigrad/equipment_system/sh_equipment.lua
@@ -274,7 +274,7 @@ local function protec(org, bone, dmg, dmgInfo, placement, boneindex, dir, hit, r
 		return
 	end
     
-    if not org.oldDmgInfo or (org.oldDmgInfo != dmgInfo) or !armor.nodamagetypeChange then
+    if not org.oldDmgInfo or (org.oldDmgInfo != dmgInfo) or armor.SideLinks and !armor.nodamagetypeChange then
         dmgInfo:SetDamageType(DMG_CLUB)
         dmgInfo:SetDamageForce(dmgInfo:GetDamageForce() * protectionDamageMul)
         dmgInfo:ScaleDamage(protectionDamageMul)
